f1d69c071193f56903ef4db2520b332eefb2e38b
xiwan <5155280@qq.com>
2019-08-21 18:25:48 +0800

    custom hero by CreateHeroForPlayer

--- game/dota1v5/scripts/vscripts/addon_game_mode.lua
+++ game/dota1v5/scripts/vscripts/addon_game_mode.lua
@@ -11,14 +11,15 @@ function Precache( context )
 			PrecacheResource( "soundfile", "*.vsndevts", context )
 			PrecacheResource( "particle", "*.vpcf", context )
 			PrecacheResource( "particle_folder", "particles/folder", context )
+	
+			PrecacheUnitByNameSync("npc_dota_hero_tinker", context)
+			PrecacheUnitByNameSync("npc_dota_hero_antimage", context)
+			PrecacheUnitByNameSync("npc_dota_hero_crystal_maiden", context)
+			PrecacheUnitByNameSync("npc_dota_hero_nevermore", context)
+			PrecacheUnitByNameSync("npc_dota_hero_juggernaut", context)
+			PrecacheUnitByNameSync("npc_dota_hero_pudge", context)
+			PrecacheUnitByNameSync("npc_dota_hero_earthshaker", context)
 	]]
-	PrecacheUnitByNameSync("npc_dota_hero_tinker", context)
-	PrecacheUnitByNameSync("npc_dota_hero_antimage", context)
-	PrecacheUnitByNameSync("npc_dota_hero_crystal_maiden", context)
-	PrecacheUnitByNameSync("npc_dota_hero_nevermore", context)
-	PrecacheUnitByNameSync("npc_dota_hero_juggernaut", context)
-	PrecacheUnitByNameSync("npc_dota_hero_pudge", context)
-	PrecacheUnitByNameSync("npc_dota_hero_earthshaker", context)
 end
 
 -- Create the game mode when we activate
@@ -29,55 +30,67 @@ end
 
 function _G.Dota1V5:InitGameMode()
 	print( "Template addon is loaded." )
+	local gamemode = GameRules:GetGameModeEntity()
+	self.gamemode = gamemode
+
 	--监听游戏进度
 	ListenToGameEvent("game_rules_state_change", Dynamic_Wrap(_G.Dota1V5, "OnGameRulesStateChange"), self) 
 
 	--监听UI事件,这是新的事件管理器
-  CustomGameEventManager:RegisterListener( "SelectHero", SelectHero )
+  CustomGameEventManager:RegisterListener( "SelectHero", function(_, keys)
+		self:SelectHero(keys)
+	end)
 
-	--GameRules:GetGameModeEntity():SetThink( "OnThink", self, "GlobalThink", 2 )
+	self.gamemode:SetThink( "OnThink", self, "GlobalThink", 2 )
 end
 
 function _G.Dota1V5:OnGameRulesStateChange(keys)
 	local state = GameRules:State_Get()
+	
 	if state == DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP then
 		PlayerResource:SetCustomTeamAssignment( 0, DOTA_TEAM_GOODGUYS )
 		GameRules:SetCustomGameSetupTimeout(0)
 		GameRules:EnableCustomGameSetupAutoLaunch( false )
-
+		print("DOTA_GAMERULES_STATE_CUSTOM_GAME_SETUP")
 	elseif state == DOTA_GAMERULES_STATE_HERO_SELECTION then
-		_G.listener = CustomGameEventManager:RegisterListener( "GoNext", GoNext )
+		--_G.listener = CustomGameEventManager:RegisterListener( "GoNext", GoNext )
+		print("DOTA_GAMERULES_STATE_HERO_SELECTION")
+	elseif state == DOTA_GAMERULES_STATE_STRATEGY_TIME then
+		--CustomUI:DynamicHud_Create(-1,"MyUIButton","file://{resources}/layout/custom_game/MyUI_button.xml",nil)
+		print("DOTA_GAMERULES_STATE_STRATEGY_TIME")
+	elseif state == DOTA_GAMERULES_STATE_TEAM_SHOWCASE then
+		--CustomUI:DynamicHud_Create(-1,"MyUIButton","file://{resources}/layout/custom_game/MyUI_button.xml",nil)
+		print("DOTA_GAMERULES_STATE_TEAM_SHOWCASE")
 	elseif state == DOTA_GAMERULES_STATE_PRE_GAME then
 		--CustomUI:DynamicHud_Create(-1,"MyUIButton","file://{resources}/layout/custom_game/MyUI_button.xml",nil)
-
+		print("DOTA_GAMERULES_STATE_PRE_GAME")
 	end
 end
 
 -- Evaluate the state of the game
 function _G.Dota1V5:OnThink()
 	if GameRules:State_Get() == DOTA_GAMERULES_STATE_GAME_IN_PROGRESS then
-		--print( "Template addon script is running." )
+		print("DOTA_GAMERULES_STATE_GAME_IN_PROGRESS")
 	elseif GameRules:State_Get() >= DOTA_GAMERULES_STATE_POST_GAME then
 		return nil
 	end
 	return 1
 end
 
-function SelectHero(index, keys)
-	
-	print("xxxxxxx2")
-	print(keys.PlayerID)
-	print(keys.HeroName)
-	GameRules:GetGameModeEntity():SetCustomGameForceHero(keys.HeroName);
-	AssignHero(keys.PlayerID, keys.HeroName)
+function _G.Dota1V5:SelectHero(keys)
+	-- for i=0, PlayerResource:GetPlayerCount() do
+	-- 	local player = PlayerResource:GetPlayer(i)
+	-- 	if player then
+	-- 		CreateHeroForPlayer(keys.HeroName, player):RemoveSelf()
+	-- 	end
+	-- end
 
+	self.gamemode:SetCustomGameForceHero(keys.HeroName);
 	CustomGameEventManager:Send_ServerToPlayer(PlayerResource:GetPlayer(keys.PlayerID), "AfterTeamSelection", {})
 end
 
-function GoNext(index, keys)
-	print("xxxxxxx")
-	print(keys.PlayerID)
-	print(keys.HeroName)
+function GoNext(keys)
+	--GameRules:GetGameModeEntity():SetCustomGameForceHero(keys.HeroName);
 	AssignHero(keys.PlayerID, keys.HeroName)	
 end
 
@@ -91,7 +104,6 @@ end
 ]]
 function AssignHero( player, hero )
 	PrecacheUnitByNameAsync( hero, function()
-		print("abababa")
 		PlayerResource:ReplaceHeroWith( player, hero, 0, 0 )
 	end, player)
 end
